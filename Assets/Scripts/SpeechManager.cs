945eca4df08036f2d81a7e383da2ca9a1a024555
Nick Landry <nlandry@activenick.net>
2016-08-27 01:12:21 -0400

    Added support for gaze, tap to place on start, speech commands and spatial sound

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Rotate Model", () =>
+        {
+            // Call the OnRotateStart method on every descendant object.
+            this.BroadcastMessage("OnRotateStart");
+        });
+
+        keywords.Add("Stop Rotation", () =>
+        {
+            // Call the OnRotateStart method on every descendant object.
+            this.BroadcastMessage("OnRotateStop");
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/TankCommands.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankCommands : MonoBehaviour {
+
+    public bool isRotating = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isRotating)
+            this.transform.Rotate(new Vector3(45, 0, 0) * Time.deltaTime);
+    }
+
+    // Called by SpeechManager when the user says the "Rotate Model" command
+    void OnRotateStart()
+    {
+        isRotating = true;
+    }
+
+    // Called by SpeechManager when the user says the "Rotate Model" command
+    void OnRotateStop()
+    {
+        isRotating = false;
+    }
+
+}
